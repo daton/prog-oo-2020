3d0c868c465694176b338ccf8676c637deefbf73
juan_ <rapidclimate@gmail.com>
2020-08-06 19:43:58 -0500

    ENTREGABKE DE LA SEMANA AUTENTICACION FAKE

--- src/main/kotlin/CasoUso3.kt
+++ src/main/kotlin/CasoUso3.kt
@@ -1,7 +1,8 @@
 fun main(){
-    println("Solucion")
+    println("Solucion a entregable")
 
     var perfilA=Perfil("floky","floky@gmail.com","floky123")
+
     var autenticacion=Autenticacion()
     autenticacion.autenticar(perfilA)
     println("Autenticado con exito")
@@ -12,9 +13,19 @@ class Perfil(
    var email:String,
     var password:String
 )
-
-
+//La siguiente clase pertenece  a un grupo de clases que se llaman MODELO. LAS CLASES MODELO GENERALMNETE
+//SON LAS QUE MANEJAN LA LOGICA DE LAS CLASES ENTIDAD.
 class BaseDatosFake{
+    //Supondremos que esta clase obtiene los registro guardados de Perfiles de una BD emota en un servidor
+    //Las clases Modelo continen dos tipos de logica: La trnsaccional y la de Persistencia
+    //La transaccional implica realmente algoritmos de acceso a información.
+    //La logica de PERSISTENCIA solamente implica opreciones basicas de acceso de informacion (guardar, buscar,
+    //buscar por id, actualizar y borrar)
+    // var xx=arrayListOf<Int>(  3,
+    //							 4,
+    //							 4)
+
+
 
     fun obtenerUsuarios():ArrayList<Perfil>
             =arrayListOf(Perfil("floky", "floky@gmail.com","floky123"),
@@ -39,10 +50,10 @@ class Autenticacion{
     }
 
     fun autenticar(perfil:Perfil){
-        if(!buscar(perfil))throw  MiExcepcion("Tienes un erro de autenticacion")
+        if(!buscar(perfil))throw  AutenticacionFallidaException("No existes en la BD")
     }
 }
 
-class MiExcepcion(mensajito:String):Exception(mensajito){
+class AutenticacionFallidaException(mensajito:String):Exception(mensajito){
 
 }
